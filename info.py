b7ae22153130dc123002eda1ef3d41e1a776d2fa
MalluLuffy <123myemailaddressis@gmail.com>
2025-04-25 11:05:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+FbXP7ElKdiUyM2I1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AnimeXSaga')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CrewXSaga') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/MalluSaitama')
+OWNER_LNK = environ.get('OWNER_LNK', '@MalluSaitama')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))

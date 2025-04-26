c69edf9c1a6a81a4f051f1157fb913b7197df85f
MalluLuffy <123myemailaddressis@gmail.com>
2025-04-26 15:24:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,6 @@ SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24607450'))
 API_HASH = environ.get('API_HASH', '204768fe292a431ee267aebaa1dbbd11')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8107934937:AAGDXZ3wFOiLGD85EwVH-SaV-tzuE_a6ALU")
-APP_URI = environ.get('APP_URI', "https://illegal-josefina-123myemailaddressis-1ff633a4.koyeb.app")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.

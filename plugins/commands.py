fc42e56a0752386b5caf705a32486fa008427c36
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-03 22:36:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,8 +51,8 @@ async def start(client, message):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                
-                InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -65,8 +65,8 @@ async def start(client, message):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                
-                InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -137,8 +137,8 @@ async def start(client, message):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                
-                InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -151,8 +151,8 @@ async def start(client, message):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-               
-                InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+               InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -192,8 +192,8 @@ async def start(client, message):
                 buttons = [[
                     InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    
-                    InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                   InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                   InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
                 ],[
                     InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -206,8 +206,8 @@ async def start(client, message):
                 buttons = [[
                     InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    
-                    InlineKeyboardButton('ᴄʜᴀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK)
+                   InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
+                   InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
                 ],[
                     InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')

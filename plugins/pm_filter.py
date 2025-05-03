a3f1edb680edfdcc709fa4721096dc718c9235d5
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-03 22:56:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1829,8 +1829,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
-                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
+                InlineKeyboardButton('Oɴᴇ Pɪᴇᴄᴇ', url=OP_LNK),
+                InlineKeyboardButton('Cʜᴀᴛ Gʀᴏᴜᴘ', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
@@ -1843,8 +1843,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-                InlineKeyboardButton('✯ Oɴᴇ Pɪᴇᴄᴇ ✯', url=OP_LNK),
-                InlineKeyboardButton('♕ Cʜᴀᴛ Gʀᴏᴜᴘ ♕', url=GRP_LNK)
+                InlineKeyboardButton('Oɴᴇ Pɪᴇᴄᴇ', url=OP_LNK),
+                InlineKeyboardButton('Cʜᴀᴛ Gʀᴏᴜᴘ', url=GRP_LNK)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')

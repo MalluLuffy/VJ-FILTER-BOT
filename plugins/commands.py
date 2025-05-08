1dad39b352f95a3a7f6ceb1ef52f0b60233172e8
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-08 14:17:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -458,7 +458,7 @@ async def start(client, message):
                     )
                     return
             if STREAM_MODE == True:
-               button = [[InlineKeyboardButton('sᴛʀᴇᴀᴍ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ', callback_data=f'generate_stream_link:{file_id}')]]
+                button = [[InlineKeyboardButton('sᴛʀᴇᴀᴍ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ', callback_data=f'generate_stream_link:{file_id}')]]
                 reply_markup=InlineKeyboardMarkup(button)
             else:
                 reply_markup = None

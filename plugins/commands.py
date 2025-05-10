fea2ccd7baa083f1e45fa3a41566e7916d27504f
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-10 19:52:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -28,11 +28,15 @@ async def start(client, message):
         pass
 
     if len(message.command) == 2 and message.command[1].startswith('getfile'):
-        searches = message.command[1].split("-", 1)[1] 
+        searches = message.command[1].split("-", 1)[1]
         search = searches.replace('-', ' ')
-        message.text = search 
+        
+        # Override the text and command so it behaves like a normal query
+        message.text = search
+        message.command = [search]
+
         ai_search = True
-        reply_msg = await message.reply_text(f"<b><i>Searching For {message.text} 🔍</i></b>")
+        reply_msg = await message.reply_text(f"<b><i>Searching For {search} 🔍</i></b>")
         await auto_filter(client, message.text, message, reply_msg, ai_search)
 
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:

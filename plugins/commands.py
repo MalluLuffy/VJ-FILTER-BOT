b40fe1264d79cc81009b00f8b33b0fa5e8d34846
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-10 17:24:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save
 from database.connections_mdb import active_connection
 from plugins.pm_filter import auto_filter
 from urllib.parse import quote_plus
-from Naman.util.file_properties import get_name, formate_file_name, get_hash, get_media_file_size
+from TechVJ.util.file_properties import get_name, formate_file_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}

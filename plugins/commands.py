0b88aa9e1a789b49f20659e4047ffff97079aea0
MalluLuffy <123myemailaddressis@gmail.com>
2025-05-18 14:31:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,8 +64,9 @@ async def start(client, message):
         if PREMIUM_AND_REFERAL_MODE == True:
             buttons = [[
                 InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-                InlineKeyboardButton('✯ Cʜᴀᴛ Gʀᴏᴜᴘ ✯', url=GRP_LNK)
+            ],[ 
+                InlineKeyboardButton('〄 Cʜᴀᴛ Gʀᴏᴜᴘ', url=GRP_LNK)
+                InlineKeyboardButton('⌬ Cʜᴀᴛ Gʀᴏᴜᴘ', url=SUPPORT_CHAT)
             ],[
                 InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                 InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
